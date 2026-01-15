6c8360307e6c16cd07449837e9c2fe75988176b9
Mila1213 <tu_correo@ejemplo.com>
2026-01-15 18:19:16 -0500

    Modificacion en paquete de service

--- src/main/java/com/example/demo/repository/EstudianteRepository.java
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repository;
 
-public interface EstudianteRepository {
+import com.example.demo.model.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+
+
 }
--- src/main/java/com/example/demo/service/EstudianteService.java
+++ src/main/java/com/example/demo/service/EstudianteService.java
@@ -1,4 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Estudiante;
+import com.example.demo.repository.EstudianteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EstudianteService {
+
+    @Autowired
+    private EstudianteRepository repository;
+
+    public Estudiante guardar(Estudiante estudiante) {
+        return repository.save(estudiante);
+    }
 }
