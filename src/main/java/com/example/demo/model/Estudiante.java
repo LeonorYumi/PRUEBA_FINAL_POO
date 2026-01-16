951b9ee05b24d4db92e0cd786801ff912e8807fa
Mila1213 <tu_correo@ejemplo.com>
2026-01-15 22:00:38 -0500

    Modificaciones en el model

--- src/main/java/com/example/demo/model/Estudiante.java
+++ src/main/java/com/example/demo/model/Estudiante.java
@@ -6,46 +6,81 @@ import jakarta.validation.constraints.*;
 @Entity
 @Table(name = "estudiantes")
 public class Estudiante {
-
+//id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+//cedula
+    @NotBlank(message = "La cédula es obligatoria")
+    @Size(min = 10, max = 10, message = "La cédula debe tener 10 dígitos")
+    @Column(unique = true)
+    private String cedula;
+//nombre
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, max = 50)
     private String nombre;
-
-    @NotBlank @Email
+//correo
+    @NotBlank
+    @Email(message = "Correo no válido")
     private String email;
-
-    @NotNull @Min(16) @Max(60)
+//edad
+    @NotNull
+    @Min(16)
+    @Max(60)
     private Integer edad;
-
+//carrera
     @NotBlank
     private String carrera;
 
     // Constructor vacío
     public Estudiante() {}
 
-    // Getters y Setters
+    // GETTERS Y SETTERS
     public Long getId() {
-        return id; }
+        return id;
+    }
+
     public void setId(Long id) {
-        this.id = id; }
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
     public String getNombre() {
-        return nombre; }
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
-        this.nombre = nombre; }
+        this.nombre = nombre;
+    }
+
     public String getEmail() {
-        return email; }
+        return email;
+    }
+
     public void setEmail(String email) {
-        this.email = email; }
+        this.email = email;
+    }
+
     public Integer getEdad() {
-        return edad; }
+        return edad;
+    }
+
     public void setEdad(Integer edad) {
-        this.edad = edad; }
+        this.edad = edad;
+    }
+
     public String getCarrera() {
-        return carrera; }
+        return carrera;
+    }
+
     public void setCarrera(String carrera) {
-        this.carrera = carrera; }
+        this.carrera = carrera;
+    }
 }
