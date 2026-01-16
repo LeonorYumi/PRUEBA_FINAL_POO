2b6efdbf57e5c76a898bab44a8b31a60b93f9fa6
Mila1213 <tu_correo@ejemplo.com>
2026-01-15 22:01:27 -0500

    Aumento e el controller de las funcionalidades de listar y crear

--- src/main/java/com/example/demo/controller/EstudianteController.java
+++ src/main/java/com/example/demo/controller/EstudianteController.java
@@ -1,6 +1,29 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Estudiante;
+import com.example.demo.service.EstudianteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+//recibe peticiones
+@RestController
+@RequestMapping("/api/estudiantes")
 public class EstudianteController {
 
+    @Autowired
+    private EstudianteService estudianteService;
+
+    // Listar
+    @GetMapping
+    public List<Estudiante> listar() {
+        return estudianteService.listarTodos();
+    }
+
+    // Crear
+    @PostMapping
+    public Estudiante crear(@Valid @RequestBody Estudiante estudiante) {
+        return estudianteService.guardar(estudiante);
+    }
 }
--- src/main/java/com/example/demo/repository/EstudianteRepository.java
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -3,9 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//Se encarga del acesso a la BD
 @Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
-
-
 }
