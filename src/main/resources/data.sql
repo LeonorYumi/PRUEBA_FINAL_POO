2a806e01bec3d454edf814ab2051f220762c7424
Mila1213 <tu_correo@ejemplo.com>
2026-01-21 12:08:51 -0500

    Creacion de la data precargada

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO estudiantes (cedula, nombre, email, edad, carrera) VALUES
+                                                                   ('0102030405', 'Juan Perez', 'juan@mail.com', 20, 'Sistemas'),
+                                                                   ('0607080910', 'Maria Gomez', 'maria@mail.com', 22, 'Software'),
+                                                                   ('1112131415', 'Carlos Ruiz', 'carlos@mail.com', 25, 'Computación');
