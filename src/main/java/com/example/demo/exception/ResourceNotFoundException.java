4e27195f5f0c3ff708890b4aaab43b4c9742e6ec
Mila1213 <tu_correo@ejemplo.com>
2026-01-24 14:56:35 -0500

    .

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.example.demo.exception;
 // Manejo de excepciones
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String mensaje) {
+
         super(mensaje);
     }
 }
